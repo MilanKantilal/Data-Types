5415ba7e60401915b79da14ea1788015d582748a
Milan Kantilal <mkantilal679@gmail.com>
2022-07-21 20:47:42 +0100

    change the nubmer 10 to 17 and 5 to 4

---
+++ DataTypes.java
@@ -0,0 +1,54 @@
+package DataTypes;
+
+public class DataTypes
+{
+
+    public static void main(String[] args)
+    {
+
+//Integer Family
+
+//Integer
+        int A=10;
+        int B=5;
+        System.out.println("A="+A +' '+ "\n"+"B="+B );
+
+//Short
+       short C=-2000;
+       System.out.println("C=" +C);
+
+//Long
+        long D=100000000L;
+        System.out.println("D=" +D);
+
+//Byte
+        byte E=115;
+        System.out.println("E="+E);
+
+
+
+// Float Family
+
+//Double
+        double F=199.99;
+        System.out.println("F="+F);
+
+//Float
+        float G=999.333f;
+        System.out.println("G="+G);
+
+//Character
+        char H='S';
+        System.out.println("H="+'S');
+
+//Boolean
+        boolean I=true;
+        boolean J=false;
+        System.out.println("I="+I +" " + "\n" + "J=" +J);
+
+//String
+        String K="Eat,Sleep and Testing";
+        System.out.println("K="+"Eat,Sleep and Testing");
+
+    }
+}
